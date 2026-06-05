05f72480e20a649a61b4ccd7b3717b8d3d024a59
cheihkaouimahdi-hub <cheihkaouimahdi@gmail.com>
2026-06-05 13:39:56 -0700

    final version 2

---
+++ apps/api/prisma/migrations/20240605_init/migration.sql
@@ -0,0 +1,41 @@
+﻿-- CreateSchema
+CREATE SCHEMA IF NOT EXISTS "public";
+
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('EMPLOYEE', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'EMPLOYEE',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Employee" (
+    "id" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "LastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "department" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'EMPLOYEE',
+    "status" TEXT NOT NULL DEFAULT 'active',
+    "deletedAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Employee_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Employee_email_key" ON "Employee"("email");
+
